a57fd5c2e723cfb9052071ea23d72789fe5a5586
Igor <garkol@bk.ru>
2022-10-01 20:29:51 +0300

    Task02

---
+++ Task02.py
@@ -0,0 +1,16 @@
+#Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем
+#первый и последний элемент, второй и предпоследний и т.д.
+
+N = int(input("Введите размер списка (целое число): "))
+while N < 1:
+    N = int(input("Введите размер списка (целое число): "))
+from random import randint
+numbers = []
+for i in range(N):
+    numbers.append(randint(-N, N))
+print(numbers)
+
+composition = []
+for i in range(N // 2 + 1):
+    composition.append(numbers[i] * numbers[N - i - 1])
+print(composition)
